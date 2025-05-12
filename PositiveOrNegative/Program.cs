2455608b32d4301be0675bfacafe636ecc81198e
AmirHosseinNarimaniRad <Amir.hossein.narimani.rad@gmail.com>
2025-05-12 16:10:24 +0330

    First Push

---
+++ PositiveOrNegative/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveOrNegative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number : ");
+
+            float num = float.Parse(Console.ReadLine());
+
+            if (num == 0)
+            {
+                Console.WriteLine("Your number is Zero!");
+
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("Your number is positive !  ");
+            }
+            else {
+                Console.WriteLine("your number is Negative");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
